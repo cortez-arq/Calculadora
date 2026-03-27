9d2ff82641b18aff6704edda6a9af6f72b44f12a
Pedro C <pedro.cortez@aluno.impacta.edu.br>
2026-03-27 11:06:03 -0300

    Função subtração

--- cal.py
+++ cal.py
@@ -1,10 +1,14 @@
 
 def soma(a,b):
     return a+b
+def subtração(a,b):
+    return a-b
 def main():
     a=int(input())
     b=int(input())
-    print(f'soma ={soma(a,b)}')
-    return a, b
+    print(f'soma = {soma(a,b)}')
+    print(f'sosubtração = {subtração(a,b)}')
+    
+    
 
 main()
