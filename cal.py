3952dcb33b43a99738412bb8484ed7537742de97
Pedro C <pedro.cortez@aluno.impacta.edu.br>
2026-03-27 11:39:32 -0300

    Função multiplicação adicionada

--- cal.py
+++ cal.py
@@ -3,11 +3,15 @@ def soma(a,b):
     return a+b
 def subtração(a,b):
     return a-b
+
+def multiplicação(a,b):
+    return a*b
 def main():
     a=int(input())
     b=int(input())
     print(f'soma = {soma(a,b)}')
     print(f'sosubtração = {subtração(a,b)}')
+    print(f' multiplicação = {multiplicação(a,b)}')
     
     
 
